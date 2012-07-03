9c2d0bfd15e7223d939c1383a1a8ae03479e297c
Chintan Parikh <chintan@thefrontiergroup.com.au>
2012-07-03 10:29:11 +0800

    get_gists working

--- js/services/github.js
+++ js/services/github.js
@@ -1,9 +1,12 @@
 var get_gists = function (user) {
-  url = 'https://api.github.com/users/' + user + '/gists';
+  var url = 'https://api.github.com/users/' + user + '/gists';
 
-  $.getJSON( url, function(data) {
-    console.log(data);
+  $.ajax({
+      url: url,
+      dataType: 'jsonp',
+
+      success: function(data) {
+      console.log(data);
+    }
   });
-
-  console.log(url);
 };
